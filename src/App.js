465c2334ca8a280e97857711f8064609774a516d
Celine Seghbossian <celineblaine@icloud.com>
2020-10-10 18:05:05 -0700

    12:30 added basic useEffect and useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const APP_ID = 'b13bf049';
+  const APP_KEY = '30b97669ea8643014a5967d0e402ba50';
+
+  const EXAMPLE_REQ = 
+  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  {/* The State Hook */}
+  const [counter, setCounter] = useState(0);
+
+  {/* The Effect Hook lets you perform side effects in function components.*/}
+  {/*The second parameter (the empty brackets) keeps useEffect from running every single time our page re-renders */}
+
+  useEffect(() => {
+    console.log("Effect has been run once!");
+  }, []);
+
+  return(
+    <div className="app">
+      {/* We assign a className to each HTML element */}
+      <form className="search-form">
+        <input className="search-bar" type='text'/>
+        <button className="search-button" type='Submit'>Search Recipes</button>
+      </form>
+      <h2 onClick = {() => setCounter(counter+1)}>{counter}</h2>
     </div>
   );
 }
